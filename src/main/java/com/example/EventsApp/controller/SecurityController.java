e5138433e5c21dce9748409a06bba22c48394b23
nnurhussien04 <nnurhussien03@gmail.com>
2025-05-20 02:12:52 +0100

    I have added google oauth2 in the backend application and added spring security - a form login needed to sign into the application

---
+++ src/main/java/com/example/EventsApp/controller/SecurityController.java
@@ -0,0 +1,20 @@
+package com.example.EventsApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+
+    @GetMapping("/")
+    public String home(){
+        return "Hello home";
+    }
+
+    @GetMapping("/secured")
+    public String secured(){
+        return "Hello, Secured";
+    }
+
+
+}
